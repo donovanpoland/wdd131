0b2da9cf75ac42434bac89f2be5936b8b923f1b3
donovanpoland <dpoland@byupathway.edu>
2025-05-30 20:32:52 -0600

    updated last modified logic

--- scripts/place.js
+++ scripts/place.js
@@ -46,12 +46,48 @@ function updateImageTitle() {
 
 // get the current year and the last date/time the page was modified
 function getYearAndLastModified() {
-    const currentYear = document.querySelector("#current-year");
-    const lastModified = document.querySelector("#last-modified");
-    const today = new Date();
+  const currentYear = document.querySelector("#current-year");
+  const lastModified = document.querySelector("#last-modified");
+  
+  const today = new Date();
+  const lastMod = new Date(document.lastModified);
 
-    currentYear.innerHTML = today.getFullYear();
-    lastModified.textContent = `Last modified: ${document.lastModified}`;
+  //check current year id is found on page
+  if (currentYear) {
+    //display the info
+    currentYear.textContent = today.getFullYear();
+  }
+
+  //check if last modified id is found the page
+  if (lastModified) {
+    // Format date to MM/DD/YYYY HH:MM:SS (local)
+    const localFormatted = lastMod.toLocaleString("en-US", {
+      year: "numeric",
+      month: "2-digit",
+      day: "2-digit",
+      hour: "2-digit",
+      minute: "2-digit",
+      second: "2-digit",
+      hour12: false,
+    });
+
+    // UTC time
+    const utcFormatted = lastMod.toLocaleString("en-US", {
+      year: "numeric",
+      month: "2-digit",
+      day: "2-digit",
+      hour: "2-digit",
+      minute: "2-digit",
+      second: "2-digit",
+      hour12: false,
+      timeZone: "UTC",
+    });
+
+    //display the info
+    lastModified.innerHTML =
+      `Last modified: ${localFormatted} GMT<br>` +
+      `Last modified: ${utcFormatted} UTC`;
+  } 
 }
 
 // calculate the windchill factor
